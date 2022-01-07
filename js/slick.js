a0004d8f3356579a68a0fc78fb8d279828a5b762
Kalecew <48532006+Kalecew@users.noreply.github.com>
2022-01-07 18:02:10 +0300

    finish slider-previews

---
+++ js/slick.js
@@ -0,0 +1,11 @@
+// https://kenwheeler.github.io/slick/
+
+	$('#slick-previews').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		adaptiveHeight: true,
+		fade: true
+	});
