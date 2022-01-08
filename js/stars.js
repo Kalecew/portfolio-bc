27be6a9c0650a52677c74aa95169a61c609d01e9
Kalecew <48532006+Kalecew@users.noreply.github.com>
2022-01-08 09:42:37 +0300

    finish stars

---
+++ js/stars.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('[data-slider-reviews-rate]').each(function(){
+		const rate = parseFloat($(this).text())
+		const parent = $(this).closest('[data-rating')
+		const overlayStars = $('[data-stars-overlay',parent)
+		const overlayWidth = rate * 100 / 5;
+		overlayStars.css("width",overlayWidth+"%")
+		console.log(parent)
+	})
+	
+})
