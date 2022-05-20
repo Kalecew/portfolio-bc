8db50e9a423baee5b7a796d03e7f52b70ddaa274
Kalecew <48532006+Kalecew@users.noreply.github.com>
2022-05-20 16:00:33 +0300

    fix bug to stars.js

--- js/stars.js
+++ js/stars.js
@@ -1,8 +1,8 @@
 $(function(){
 	$('[data-slider-reviews-rate]').each(function(){
 		const rate = parseFloat($(this).text())
-		const parent = $(this).closest('[data-rating')
-		const overlayStars = $('[data-stars-overlay',parent)
+		const parent = $(this).closest('[data-rating]')
+		const overlayStars = $('[data-stars-overlay]',parent)
 		const overlayWidth = rate * 100 / 5;
 		overlayStars.css("width",overlayWidth+"%")
 	})
